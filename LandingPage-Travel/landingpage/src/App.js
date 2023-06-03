5c5b1b039b8afcf83705593f146ef3a3c4b740ac
Allisson Matheus <allisson.boiane@outlook.com>
2023-06-03 17:23:03 -0300

    create Banner component

--- LandingPage-Travel/landingpage/src/App.js
+++ LandingPage-Travel/landingpage/src/App.js
@@ -1,10 +1,12 @@
 import './root.css'
 import './reset.css'
+import Banner from './components/Banner';
+
 
 function App() {
   return (
     <div className="App"> 
-      
+      <Banner/>
     </div>
   );
 }
---
+++ LandingPage-Travel/landingpage/src/components/Banner/index.js
@@ -0,0 +1,14 @@
+import styles from './styles.module.css'
+// import imgWaterFall from '../../assets/img/waterfall.png'
+
+function Banner () {
+    return (
+        <div className={styles.bannerContainer}>
+            <div className={styles.shadowImage}>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Banner
